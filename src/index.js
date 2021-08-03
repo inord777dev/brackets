c4c66577bdce20f847debaa119e210bc478fc7dd
inord777dev <inord777dev@gmail.com>
2021-08-03 17:13:43 +0300

    init: start interested task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = str;
+  bracketsConfig.forEach(element => {
+    let length = result.length + 2;
+    let index1, index2;
+    while (result.length > 0 && length - result.length == 2) {
+        length = result.length;
+        index1 = result.indexOf(element[0]);
+        index2 = result.indexOf(element[1]);
+        if (index2 > index1) {
+          result = result.replace(element[0], '');
+          result = result.replace(element[1], '');
+        }
+    }
+  });
+  return result == '';
 }
