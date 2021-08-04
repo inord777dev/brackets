4b6a1a27bcb6054bb9e3e4f5f09e044c61e7b3eb
inord777dev <inord777dev@gmail.com>
2021-08-04 11:25:38 +0300

    refactor: complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  let result = str;
-  bracketsConfig.forEach(element => {
-    let length = result.length + 2;
-    let index1, index2;
-    while (result.length > 0 && length - result.length == 2) {
-        length = result.length;
-        index1 = result.indexOf(element[0]);
-        index2 = result.indexOf(element[1]);
-        if (index2 > index1) {
-          result = result.replace(element[0], '');
-          result = result.replace(element[1], '');
-        }
+  let items = str.split('');
+  bracketsConfig.forEach((element, index) => {
+    let rank = (index + 1) / 10;
+    let position1 = items.indexOf(element[0]);
+    let position2 = items.indexOf(element[1], position1 + 1);
+    while (position1 != -1 || position2 != -1) {
+      if (position1 != -1) {
+        items[position1] = rank;
+      }
+      if (position2 != -1) {
+        items[position2] = - rank;
+      }
+      position1 = items.indexOf(element[0]);
+      position2 = items.indexOf(element[1], position1 + 1);
     }
   });
-  return result == '';
+  let index = 0, sum =[];
+  while (index < items.length) {
+    if (items[index] > 0) {
+      sum.push(items[index]);
+    } else {
+      if (sum.length == 0 || sum.pop() + items[index] != 0) {
+        break;
+      }
+    }
+    index++;
+  }
+  return index == items.length && sum.length == 0;
 }
+
+
